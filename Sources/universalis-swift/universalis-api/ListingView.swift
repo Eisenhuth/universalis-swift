228084c5283ba897403eff66a7ed1e319ffb4f0c
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2023-12-04 00:55:46 +0100

    Update ListingView.swift
    - added retainerCityName

--- Sources/universalis-swift/universalis-api/ListingView.swift
+++ Sources/universalis-swift/universalis-api/ListingView.swift
@@ -22,9 +22,27 @@ public struct ListingView: Codable, Identifiable {
 }
 
 public extension ListingView{
-    var id: Int{
-        listingID?.hashValue ?? UUID().hashValue
-    }
     
-    //TODO: retainerCityName
+    var id: Int{ listingID?.hashValue ?? UUID().hashValue }
+    
+    var retainerCityName: String {
+        switch retainerCity {
+        case 1:
+            return "Limsa Lominsa"
+        case 2:
+            return "Gridania"
+        case 3:
+            return "Ul'dah"
+        case 4:
+            return "Ishgard"
+        case 7:
+            return "Kugane"
+        case 10:
+            return "Crystarium"
+        case 12:
+            return "Old Sharlayan"
+        default:
+            return ""
+        }
+    }
 }
