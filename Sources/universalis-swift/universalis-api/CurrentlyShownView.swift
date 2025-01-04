ee978d4a47ab0b96c502d57fa340a0de40e1c0bd
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2025-01-04 16:43:29 +0100

    added hasData

--- Sources/universalis-swift/universalis-api/CurrentlyShownView.swift
+++ Sources/universalis-swift/universalis-api/CurrentlyShownView.swift
@@ -32,7 +32,7 @@ public struct CurrentlyShownView: Codable, Identifiable {
     public let recentHistoryCount: Int
     public let unitsForSale: Int
     public let unitsSold: Int
-    
+    public let hasData: Bool
 }
 
 public extension CurrentlyShownView{
